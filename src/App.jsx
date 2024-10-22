9dff2724e96d3c8aaa1b29cb83dbf6beb1d7c0b8
ZenWeb3 <samueledohoeket257@gmail.com>
2024-10-22 16:16:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styles from './style'
+
+const App = () => {
+ return(
+      <div className="w-full overflow-hidden bg-primary">
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+               <div className={`${styles.boxWidth}`}>
+                    Navbar
+               </div>
+          </div>
+
+          <div className={`${styles.flexStart} bg-primary`}>
+               <div className={`${styles.boxWidth}`}>
+                    Hero
+               </div>
+          </div> 
+
+
+          <div className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
+               <div className={`${styles.boxWidth}`}>
+                    Stats
+                    Business
+                    Billing
+                    CarDeal
+                    Testimonial
+                    Clients
+                    CTA
+                    Footer
+               </div>
+          </div>
+      </div>
+ )
+  
+}
+
+export default App
