f6a7b3c50fe52c7c0cad037af5783f3b91c5409f
ZenWeb3 <samueledohoeket257@gmail.com>
2024-11-02 22:15:35 +0100

    Last commit

--- src/Constant/index.js
+++ src/Constant/index.js
@@ -1,5 +1,7 @@
 import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
 
+// Links for mapping 
+
 export const navLinks = [
   {
     id: "home",
